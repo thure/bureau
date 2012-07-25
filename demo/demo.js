0cbbe9f2ce0f0e895b289d93b14a229bd8453cc9
willshown <w@willshown.com>
2012-07-25 15:19:49 -0400

    Added support for common “strings-as-functions” and regular expressions in configuration. Modularized code a ton. Expanded demo.html to show some of the new features. Should have checked in one feature at a time, really.

--- demo/demo.js
+++ demo/demo.js
@@ -1,22 +1,39 @@
 // Bureau v0.1 - bureaujs.org/license - Will Shown | w@willshown.com
 
-;(function(){
+(function ($) {
 
   console.log('Demo started.');
 
+  //here's the full callback definition case:
+//  $('fieldset.bureau-target').bureau({
+//    '.depends-on-option1': {
+//      dependsOn: '#option1',
+//      'when': function ($watchedElem) {
+//        return $watchedElem.prop('checked');
+//      },
+//      'then': function () {
+//        $(this).show();
+//      },
+//      'else': function () {
+//        $(this).hide();
+//      }
+//    }
+//  });
+
+  //here's what it should look like for simple showing:
   $('fieldset.bureau-target').bureau({
     '.depends-on-option1': {
-      dependsOn: '#option1',
-      when: function($watchedElem){
-        return $watchedElem.prop('checked');
-      },
-      then: function(){
-        $(this).show();
-      },
-      else: function(){
-        $(this).hide();
-      }
+      'dependsOn': '#option1',
+      'when': 'checked',
+      'then': 'show',
+      'else': 'hide'
+    },
+    '.depends-on-textinput2': {
+      'dependsOn': '#textinput2',
+      'when': /^[\d\s]+$/,
+      'then': 'show',
+      'else': 'hide'
     }
   });
 
-}());
+})(typeof Zepto !== "undefined" && Zepto !== null ? Zepto : jQuery);
