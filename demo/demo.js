cae651c7326c68ec5c401f03e3f7a091cc8b3772
willshown <w@willshown.com>
2012-08-07 19:56:21 -0400

    Changed 'then' and 'else' to 'everyYup' and 'everyNope' respectively, and added 'yup' and 'nope' callbacks for when and only when validation changes.

--- demo/demo.js
+++ demo/demo.js
@@ -8,34 +8,34 @@
     '.depends-on-option1': {
       'dependsOn': '#option1',
       'when': 'checked',
-      'then': 'show',
-      'else': 'hide'
+      'yup': 'show',
+      'nope': 'hide'
     },
     '.depends-on-textinput2': {
       'dependsOn': '#textinput2',
       'when': /^[\d\s]+$/,
-      'then': 'show',
-      'else': 'hide'
+      'yup': 'show',
+      'nope': 'hide'
     },
     '#textinput4': {
       'dependsOn': '#textinput4',
       'rules': [
         {
           'when': 'empty',
-          'then': function(){
+          'yup': function(){
             $(this).css('background-color','white');
           }
         },
         {
           'when': /^[0-9A-F]{3}$/i,
-          'then': function(){
+          'yup': function(){
             var self = this;
             $(this).css('background-color','#'+$(self).val());
           }
         },
         {
           'when': /^[0-9A-F]{6}$/i,
-          'then': function(){
+          'yup': function(){
             var self = this;
             $(this).css('background-color','#'+$(self).val());
           }
